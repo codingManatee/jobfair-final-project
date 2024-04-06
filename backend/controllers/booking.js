9d334d16f5956780cdeb94639628adb67d5abbf0
WorkingDada <6430222521@student.chula.ac.th>
2024-04-07 04:24:01 +0700

    WIP

--- backend/controllers/booking.js
+++ backend/controllers/booking.js
@@ -1,5 +1,5 @@
 const Booking = require('../models/Booking')
-
+const Company = require('../models/Company')
 
 // @desc    Get a booking 
 // @route   GET /api/v1/booking/:id
@@ -25,8 +25,30 @@ exports.getBooking = async (req,res,next) => {
 }
 
 // @desc    Create a booking
-// @route   POST /api/v1/booking
-// @access  Public
+// @route   POST /api/v1/companies/:companyId/bookings
+// @access  Private
 exports.createBooking = async (req,res,next) => {
-    
+    try {
+        console.log(req.params)
+        req.body.company = req.params.companyId;
+        const company = await Company.findById(req.params.companyId);
+        if (!company) {
+            return res.status(400).json({ success : false , message : "Company does not exists"});
+        }
+        req.body.user = req.params.user.id;
+
+        const existedBookings = await Booking.find({ user : req.user.id });
+
+        if (existedBookings >= 3) {
+            return res.status(400).json({ success : false , message : "Cannot create more than 3 bookings"});
+        }
+
+        const booking = Booking.create(req.body);
+        if (!booking) {
+            return res.status(400).json({ success : false , message : "Cannot create booking"});
+        }
+        res.status(201).json({ success : true , data : booking });
+    } catch (err) {
+        res.status(400).json({ success : false })
+    }
 }
--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getBooking } = require('../controllers/booking');
+const { getBooking, createBooking } = require('../controllers/booking');
 const router = express.Router();
 
 const { protect , authorization } = require('../middleware/auth')
 
 router.route('/booking/:id').get(getBooking);
+router.route('/booking').post(protect, createBooking);
 
 module.exports = router;
