90130ff3cbd99d72dfa7bdfdeb998191b00a7c76
Bobby Punnawich <97863019+BobbyPunnawich@users.noreply.github.com>
2024-04-07 23:46:05 +0700

    WIP

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,86 +1,92 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 const sendTokenResponse = (user, statusCode, res) => {
-    // Create Token
-    const token = user.getSignedJwtToken();
-    const options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly : true
-    };
-    if (process.env.NODE_ENV == 'production') {
-        options.secure = true
-    }
-    res.status(statusCode).cookie("token",token,options).json({
-        success : true,
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        token
-    })
+  // Create Token
+  const token = user.getSignedJwtToken();
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV == "production") {
+    options.secure = true;
+  }
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    token,
+  });
 };
 
 // @desc    Register a user
 // @route   POST /api/v1/auth/register
 // @access  Public
-exports.register = async (req,res,next) => {
-    try {
-        const { name , telephone , email , password } = req.body;
-        const user = await User.create({
-            name,
-            telephone,
-            email,
-            password
-        });
-        res.status(201).json({ success : true , user});
-        }
-    catch(err) {
-        res.status(400).json({ success : false, message : err.message});
-        console.log(err);
-    }
+exports.register = async (req, res, next) => {
+  try {
+    const { name, telephone, email, password } = req.body;
+    const user = await User.create({
+      name,
+      telephone,
+      email,
+      password,
+    });
+    res.status(201).json({ success: true, user });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+    console.log(err);
+  }
 };
 
 // @desc    Login a user
 // @route   POST /api/v1/auth/login
 // @access  Public
-exports.login = async (req,res,next) => {
-    try {
-        const { email , password } = req.body;
-        if (!email || !password) {
-            return res.status(400).json({ success : false , error : "Please provide email and password"});
-        }
-        const user = await User.findOne({ email }).select("+password");
-        if (!user) {
-            return res.status(401).json({ success : false , error : "No user in database"});
-        }
-        const isMatch = user.matchPassword(password);
-        if (!isMatch) {
-            return res.status(401).json({ success : false , error : "Password did not match"});
-        }
-        sendTokenResponse(user,200,res);
-        // res.status(200).json({ success : true , message : "Login completed"});
-    } catch (err) {
-        console.log(err)
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Please provide email and password" });
     }
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, error: "No user in database" });
+    }
+    const isMatch = user.matchPassword(password);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Password did not match" });
+    }
+    sendTokenResponse(user, 200, res);
+    // res.status(200).json({ success : true , message : "Login completed"});
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // @desc    Logout a user
 // @route   GET /api/v1/auth/logout
 // @access  Private
-exports.logout = async (req,res,next) => {
-    res.clearCookie('token');
-    res.status(200).json({ success : true , messsage : "Logged out successfully"})
-}
+exports.logout = async (req, res, next) => {
+  res.clearCookie("token");
+  res.status(200).json({ success: true, messsage: "Logged out successfully" });
+};
 
 // @desc    Get current logged in user
 // @route   GET /api/v1/auth/me
 // @access  Private
-exports.getMe = async (req,res,next) => {
-    try {
-        const user = await User.findById(req.user.id);
-        res.status(200).json({ success : true , data : user })
-    } catch (err) {
-        res.status(400).json({ success : false })
-        console.log(err)
-    }
+exports.getMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+    res.status(200).json({ success: true, data: user });
+  } catch (err) {
+    res.status(400).json({ success: false });
+    console.log(err);
+  }
 };
-
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,36 +1,46 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 // Protect routes
-exports.protect = async (req,res,next) => {
-    let token;
+exports.protect = async (req, res, next) => {
+  let token;
 
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(' ')[1];
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token || token == "null") {
+    return res.status(401).json({ success: false, error: "Token is null" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (err) {
+    return res
+      .status(401)
+      .json({ success: false, error: "Not authorized to access this route" });
+  }
+};
+
+exports.authorization = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Not authorized to access this route" });
     }
 
-    if (!token || token == 'null') {
-        return res.status(401).json({ success : false , error : "Token is null"});
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        error: `User role ${req.user.role} is not authorized to access this route`,
+      });
     }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id);
-        next();
-    } catch (err) {
-        return res.status(401).json({ success : false , error : "Not authorized to access this route"});
-    }
-}
-
-exports.authorization = (... roles) => {
-    return (req,res,next) => {
-        if (!req.user) {
-            return res.status(401).json({ success :false, error : "Not authorized to access this route"});
-        }
-
-        if (!roles.includes(req.user.role)) {
-            return res.status(403).json({ success : false , error : `User role ${req.user.role} is not authorized to access this route`});
-        }
-        next();
-    }
-}
+    next();
+  };
+};
